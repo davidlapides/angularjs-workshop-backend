f89b7523928cc1977c07707e531fd38462ffe3ce
Dustin Goodman <dustin.s.goodman@gmail.com>
2015-09-13 16:19:02 -0400

    create todo model

---
+++ app/models/todo.rb
@@ -0,0 +1,5 @@
+class Todo < ActiveRecord::Base
+  validates :task, presence: true
+
+  belongs_to :user
+end
---
+++ spec/models/todo_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe Todo do
+  it 'has a valid factory' do
+    FactoryGirl.build_stubbed(:todo).should be_valid
+  end
+
+  it 'is invalid without a task' do
+    FactoryGirl.build(:todo, task: nil).should_not be_valid
+  end
+end
